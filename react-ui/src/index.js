58baa65fa29a4ca7714a47c87d8bbd64eb08a68f
chmonster <perry.moore@gmail.com>
2022-05-31 09:56:55 -0230

    initial semantic-ui install, rollback to React 17

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,12 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+//import React from 'react'
+//import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './App'
 import store from './store'
+//import 'semantic-ui-css/semantic.min.css'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+/*ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
-)
+)*/
